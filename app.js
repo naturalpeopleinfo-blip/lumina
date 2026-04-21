3f106b2da9d2c2d086651f3683fa080b164e8a65
dAisukE goRiki <naturalpeople.info@gmail.com>
2026-04-21 21:59:19 +0900

    Refine playback speed icons

--- app.js
+++ app.js
@@ -899,16 +899,12 @@
   function getSpeedIconMarkup(speed) {
     if (speed === 1) {
       return (
-        '<svg class="speed-icon speed-icon-walk" viewBox="0 0 24 24" aria-hidden="true">' +
-          '<path d="M13.1 4.3a2.2 2.2 0 1 1-4.4 0 2.2 2.2 0 0 1 4.4 0Zm-1.9 3.3c.7 0 1.4.3 1.8.9l1.5 2.2 2.1 1.1-.8 1.6-2.7-1.3-1.1-1.5-.8 3.3 2.8 2.1v4h-2v-3l-2.2-1.5-1 4.5H6.7l1.6-7 1-3.7-1.6.9-1.1 2.7-1.8-.7 1.3-3.2 3.4-2c.5-.3 1.1-.4 1.7-.4Z"></path>' +
-        "</svg>"
+        '<img class="speed-icon speed-icon-walk" src="./assets/walking.svg" alt="" aria-hidden="true">'
       );
     }
 
     return (
-      '<svg class="speed-icon speed-icon-run" viewBox="0 0 24 24" aria-hidden="true">' +
-        '<path d="M15 4.2a2.2 2.2 0 1 1-4.4 0 2.2 2.2 0 0 1 4.4 0ZM11.2 7c.8 0 1.5.3 2 .9l1.7 2 2.6.2-.1 1.9-3.5-.3-1-1.1-1.1 2.2 2.7 1.7 1.6 5.1-2 .6-1.3-4.1-2.7-1.5-2.2 5.2-1.9-.8 2.4-5.7 1.1-2.5-1.8.9-1.9 2.3-1.5-1.2 2.2-2.7 3.6-2.6c.4-.3.8-.5 1.4-.5Z"></path>' +
-      "</svg>"
+      '<img class="speed-icon speed-icon-run" src="./assets/running.svg" alt="" aria-hidden="true">'
     );
   }
 
